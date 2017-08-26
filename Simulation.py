c3d27349bad3c0c8c90d513c2c3a299a5af6e0cd
Halberd63 <hwboy2@student.monash.edu>
2017-08-26 19:30:18 +1000

    Fixed the simulation class and added getters

--- Simulation.py
+++ Simulation.py
@@ -3,33 +3,58 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 
 class Miner(Agent):
-    """An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model, power):
-        super().__init__(unique_id, model)
-        self.wealth = 1
+    
+    def __init__(self, uniqueID, model, power):
+        super().__init__(uniqueID, model)
+        self.wealth = 0
         self.power = power
-        self.id = unique_id
+        self.id = uniqueID
     
     #Each step, the miners try to solve a puzzle
     def step(self):
         print(self.id)
 
-class TheSimulation(Model):
-    """A model with some number of agents."""
-    def __init__(self, N, P, D):
-        self.num_agents = N
-        self.puzzleDifficulty = D
-        self.schedule = RandomActivation(self)
-        # Create agents
-        self.pool_power = N
-        for i in range(self.num_agents):
-            a = Miner(i, self, 1)
-            self.schedule.add(a)
+class Pool:
+    def __init__(self):
+        pass #TODO
 
+
+#The overarching simulation class.
+#Takes in number of agents, number of pools and puzzle difficulty
+class TheSimulation(Model):
+    def __init__(self, N, P, D):
+        #Handle the input arguments
+        self.numberOfMiners = N
+        self.numberOfPools = P
+        self.puzzleDifficulty = D
+
+        #Activate the schedule 
+        #(Random means that agents act in a random order each turn)
+        self.schedule = RandomActivation(self)
+        
+        #Create agents
+        for i in range(self.numberOfMiners):
+            newMiner = Miner(i, self, 1)
+            self.schedule.add(newMiner)
+
+        #Create pools
+        self.pools = [Pool() for _ in range(self.numberOfPools)]
+
+    #Advance the model by a discrete step
     def step(self):
-        '''Advance the model by one step.'''
         self.schedule.step()
+
+    #Standard accessor functions
+    def getNoOfMiners(self):
+        return self.numberOfMiners
+    def getNoOfPools(self):
+        return self.numberOfPools
+    def getPuzzleDifficulty(self):
+        return self.puzzleDifficulty
+    def getPoolList(self):
+        return self.pools
 
 
 model = TheSimulation(10,0,100)
+model.step()
 model.step()
