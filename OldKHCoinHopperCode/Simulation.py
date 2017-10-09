3b2705e242c59ecb9cc48f04add6aaccf6c12edd
Halberd63 <hwboy2@student.monash.edu>
2017-10-09 20:22:56 +1100

    Began implementing Yevhens hopping model

---
+++ OldKHCoinHopperCode/Simulation.py
@@ -0,0 +1,36 @@
+from sys import argv
+from Simulation_Classes import *
+from Simulation_Graphing import *
+
+
+
+
+
+
+#Main function for running the simulation
+#Params are pretty self explanitory 
+#(Although it is worth noting that PuzzleDifficulty will be the... 
+#...average number of cycles it will take to find each block)
+def runSimulation():
+    inFile = open("Simulation_Specs.txt", 'r')
+    model = TheSimulation(inFile)
+    inFile.close()
+    
+    #Get number of cycles
+    inFile = open("Simulation_Specs.txt", 'r')
+    for line in inFile:
+        cycles = int(line[1:line.index(',')])
+        break
+    inFile.close()
+
+    for _ in range(cycles):
+        model.step()
+    #model.showAgentDeets()
+    model.showPoolDeets()
+    return model
+
+if __name__ == "__main__":
+    theModel = runSimulation()
+    #graphBlocksFoundOverTime(theModel)
+    graphWealthoverID(theModel)
+    #graphPoweroverID(theModel)
