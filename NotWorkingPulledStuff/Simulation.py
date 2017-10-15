db61d825bc511b82f2c38f52da0ee167dde7ab38
Halberd63 <hwboy2@student.monash.edu>
2017-10-16 09:30:37 +1100

    Some simulations have been run

---
+++ NotWorkingPulledStuff/Simulation.py
@@ -0,0 +1,41 @@
+from sys import argv
+from Simulation_Classes import *
+from Simulation_Graphing import *
+import time
+
+
+
+
+
+
+#Main function for running the simulation
+#Params are pretty self explanitory 
+#(Although it is worth noting that PuzzleDifficulty will be the... 
+#...average number of cycles it will take to find each block)
+def runSimulation():
+    inFile = open("Simulation_Specs.txt", 'r')
+    model = TheSimulation(inFile)
+    inFile.close()
+    
+    #Get number of cycles
+    inFile = open("Simulation_Specs.txt", 'r')
+    for line in inFile:
+        cycles = int(line[1:line.index(',')])
+        break
+    inFile.close()
+
+    for i in range(cycles):
+        model.step()
+        if i%100 == 0:
+            #Spends too much time on printing percentage
+            print(str(100 * i / cycles) + "%")
+    #model.showAgentDeets()
+    #model.showPoolDeets()
+    model.showFocussedMinerDeets()
+    return model
+
+if __name__ == "__main__":
+    theModel = runSimulation()
+    #graphBlocksFoundOverTime(theModel)
+    graphWealthoverID(theModel)
+    #graphPoweroverID(theModel)
---
+++ NotWorkingPulledStuff/Simulation_Graphing.py
@@ -0,0 +1,161 @@
+import plotly
+plotly.tools.set_credentials_file(username='karlerikoja', api_key='tJQ6g692vFMVs89glb3v')
+import plotly.graph_objs as go
+import plotly.plotly as py
+import matplotlib.pyplot as plt
+
+
+#This graph is mostly used to prove our algorithm works correctly
+def graphBlocksFoundOverTime(model):
+    #Code for proving that the block finding is calculated correctly
+    miningTimes = model.getMiningTimes()
+    miningTimes.sort()
+    print(miningTimes)
+    numberOfBlocksOverTime = []
+    for i in range(1,miningTimes[-1]+1):
+        for j in range(len(miningTimes)):
+            if i < miningTimes[j]:
+                numberOfBlocksOverTime.append(j)
+                break
+    print(numberOfBlocksOverTime)
+
+    trace = go.Scatter(
+        x = [i for i in range(miningTimes[-1]+1)],
+        y = numberOfBlocksOverTime,
+        mode = 'markers'
+    )
+    data = [trace]
+    layout = go.Layout(
+        title = "Number of Blocks Mined over Time",
+        titlefont = dict(
+            size = 32
+        ),
+        xaxis = dict(
+            title = "Time",
+            titlefont = dict(
+                size = 18
+            )
+        ),
+        yaxis = dict(
+            title = "Number of Blocks Found Within Given Time",
+            titlefont = dict(
+                size = 18
+            )
+        )
+    )
+    py.plot(go.Figure(data = data, layout = layout)
+        , filename='Number of blocks mined vs time')
+
+
+
+
+
+
+#This graph is mostly used to prove our algorithm works correctly
+def graphWealthoverIndependance(model):
+    trace = go.Scatter(
+        x = model.getSoloMiningPower(),
+        y = model.getMinerWealth(),
+        mode = 'markers'
+    )
+    data = [trace]
+    layout = go.Layout(
+        title = "Miner Wealth vs Miner Independance",
+        titlefont = dict(
+            size = 32
+        ),
+        xaxis = dict(
+            title = "Processing Power Dedicated to Mining Alone",
+            titlefont = dict(
+                size = 18
+            )
+        ),
+        yaxis = dict(
+            title = "Wealth of the Miner",
+            titlefont = dict(
+                size = 18
+            )
+        )
+    )
+    py.plot(go.Figure(data = data, layout = layout)
+        , filename='Solo Power vs Wealth')
+
+def graphWealthoverID_Legacy(model):
+    trace = go.Scatter(
+        x=model.getMinerID(),
+        y=model.getMinerWealth(),
+        mode='markers'
+    )
+    data = [trace]
+    layout = go.Layout(
+        title="Miner Wealth vs ID",
+        titlefont=dict(
+            size=32
+        ),
+        xaxis=dict(
+            title="Miners",
+            titlefont=dict(
+                size=18
+            )
+        ),
+        yaxis=dict(
+            title="Wealth of the Miner",
+            titlefont=dict(
+                size=18
+            )
+        )
+    )
+    py.plot(go.Figure(data=data, layout=layout)
+            , filename='MinerID vs Wealth')
+
+
+def graphPoweroverID_Legacy(model):
+    trace = go.Scatter(
+        x=model.getMinerID(),
+        y=model.getMinerPower(),
+        mode='markers'
+    )
+    data = [trace]
+    layout = go.Layout(
+        title="Miner Power vs ID",
+        titlefont=dict(
+            size=32
+        ),
+        xaxis=dict(
+            title="Miners",
+            titlefont=dict(
+                size=18
+            )
+        ),
+        yaxis=dict(
+            title="Power of the Miner",
+            titlefont=dict(
+                size=18
+            )
+        )
+    )
+    py.plot(go.Figure(data=data, layout=layout)
+            , filename='MinerPower vs Wealth')
+
+
+
+
+
+def graphPoweroverID(model):
+    x=model.getMinerID()
+    y=model.getMinerPower()
+    plt.plot(x,y)
+    plt.xlabel('Miner ID')
+    plt.ylabel('Miner Power')
+    plt.title('Miner Power vs ID')
+    plt.show()
+
+def graphWealthoverID(model):
+    x=model.getMinerID()
+    y=model.getMinerWealth()
+    plt.plot(x,y, 'ro')
+    plt.xlabel('Miner ID')
+    plt.ylabel('Miner Wealth')
+    plt.title('Miner Wealth vs ID')
+    plt.show()
+        
